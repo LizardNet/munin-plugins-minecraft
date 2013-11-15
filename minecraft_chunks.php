2399f6adbad52d10592eb2e6b94b31d2f2b2f3d9
Andrew "FastLizard4" Adams <FastLizard4-Git-Commits@fastlizard4.org>
2013-11-15 14:09:33 -0800

    Add a glaring syntax error to test out Jenkins
    I will kill whomever promotes this past code review.
    
    You think I'm kidding.
    
    Change-Id: Ia49400159ca0cdd214b79e6bdc7484cc4d4a56b1

--- minecraft_chunks.php
+++ minecraft_chunks.php
@@ -1,5 +1,8 @@
 #!/usr/bin/php
 <?php
+
+awefawefHI I'M A SYNTAX ERROR!!!:!!:p!p!+o!p
+
 /*
 LIZARDNET FASTLIZARD4/MUNIN-PLUGINS/MINECRAFT <https://fastlizard4.org/wiki/Download:Minecraft_Munin_plugins>
 by FastLizard4 and the LizardNet Munin Plugins Development Team <https://gerrit.fastlizard4.org/r/#/admin/groups/17,members>
